3c1c0f3984c9a28d3e4a79cfadc6fca113ceb86b
RalphGustave <rdgustave@gmail.com>
2020-04-07 20:55:07 -0700

    adding news article component

---
+++ src/app/components/news-article/news-article.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-news-article',
+  templateUrl: './news-article.component.html',
+  styleUrls: ['./news-article.component.sass']
+})
+export class NewsArticleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
