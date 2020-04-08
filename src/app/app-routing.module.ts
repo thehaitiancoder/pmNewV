05c861bfde935ea2514dbec1e6be172b23f5d6fb
RalphGustave <rdgustave@gmail.com>
2020-04-07 20:58:16 -0700

    add routing for news article component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,17 +5,19 @@ import { LyricsComponent } from './components/lyrics/lyrics.component';
 import { ArtistsComponent } from './components/artists/artists.component';
 import { NewsComponent } from './components/news/news.component';
 import { SongComponent } from './components/song/song.component';
+import { NewsArticleComponent } from './components/news-article/news-article.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'lyrics', component: LyricsComponent},
-  {path: 'lyrics/:slug', component: SongComponent},
-  {path: 'artists', component: ArtistsComponent},
-  {path: 'news', component: NewsComponent}
+  { path: '', component: HomeComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'news/:slug', component: NewsArticleComponent },
+  { path: 'lyrics', component: LyricsComponent },
+  { path: 'lyrics/:slug', component: SongComponent },
+  { path: 'artists', component: ArtistsComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
